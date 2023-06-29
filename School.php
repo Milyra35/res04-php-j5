1c7686e3669a25e7d21c961cbfdb5a83ced38326
Milyra35 <marie.richir35@gmail.com>
2023-06-29 14:58:00 +0000

    Fin exercice composition de classes en PHP

--- School.php
+++ School.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'User.php';
+require 'Student.php';
 
 class School {
     private int $id;
@@ -25,8 +26,44 @@ class School {
     
     public function getTeacher()
     {
-        return
+        return $this->teacher;
+    }
+    public function setTeacher(User $teacher)
+    {
+        $this->teacher = $teacher;
+    }
+    
+    public function getStudents()
+    {
+        return $this->students;
+    }
+    public function setStudents(Student $students)
+    {
+        $this->students = $students;
+    }
+    
+    public function addStudent(Student $student)
+    {
+        array_push($this->students, $student);
+        return $this->students;
+    }
+    
+    public function removeStudent(Student $student)
+    {
+        $index = array_search($student, $index);
+        array_splice($this->students, $index);
+        return $this->students;
     }
 }
+
+
+$student1 = new Student("Marie", "Richir", "marie.richir@gmail.com");
+$student2 = new Student("Laureline", "Aga-Bibrac", "laureline@gmail.com");
+$teacher1 = new User("Mari", "Doucet", "mari@gmail.com");
+
+$school = new School($teacher1);
+$school->addStudent($student1);
+$school->addStudent($student2);
+var_dump($school);
 
 ?>
