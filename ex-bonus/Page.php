4d1f4bdf51c6bf399f00d43d5b447bb9627bc8c8
Milyra35 <marie.richir35@gmail.com>
2023-07-03 10:20:55 +0000

    Initialisation de classes pour la base de données modélisation

---
+++ ex-bonus/Page.php
@@ -0,0 +1,82 @@
+<?php
+
+class Page {
+    private int $id;
+    private string $name;
+    private string $title;
+    private string $content;
+    private string $route;
+    
+    public function __construct(string $name, string $title, string $content, string $route)
+    {
+        $this->name = $name;
+        $this->title = $title;
+        $this->content = $content;
+        $this->route = $route;
+        $this->id = $id;
+    }
+    
+    public function getName() : string
+    {
+        return $this->name;
+    }
+    public function setName(string $name) : void
+    {
+        $this->name = $name;
+    }
+    
+    public function getTitle() : string
+    {
+        return $this->title;
+    }
+    public function setTitle(string $title) : void
+    {
+        $this->title = $title;
+    }
+    
+    public function getContent() : string
+    {
+        return $this->content;
+    }
+    public function setContent(string $content) : void
+    {
+        $this->content = $content;
+    }
+    
+    public function getRoute() : string
+    {
+        return $this->route;
+    }
+    public function setRoute(string $route) : void
+    {
+        $this->route = $route;
+    }
+    
+    public function getId() : int
+    {
+        return $this->id;
+    }
+    public function setId(int $id) : void
+    {
+        $this->id = $id;
+    }
+    
+    public function addPage(PDO $db, Page $page)
+    {
+        $query = $db->prepare("INSERT INTO pages (name, content, title, route) 
+                       VALUES (?, ?, ?, ?)");
+        $query->execute([$page->getName(), $page->getContent(), $page->getTitle(), $page->getRoute()]);
+    }
+    
+    public function removePage(PDO $db, Page $page)
+    {
+        $query = $db->prepare("DELETE FROM pages
+                               WHERE pages.title = :name");
+        $parameters = [
+            'name' => $page->getName()   
+        ];
+        $query->execute($parameters);
+    }
+}
+
+?>
---
+++ ex-bonus/User.php
@@ -0,0 +1,81 @@
+<?php
+
+class User {
+    private int $id;
+    private string $username;
+    private string $password;
+    private string $email;
+    
+    public function __construct(string $username, string $password, string $email)
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->email = $email;
+        $this->id = -1;
+    }
+    
+    public function getUsername() : string
+    {
+        return $this->username;
+    }
+    public function setUsername(string $username) : void
+    {
+        $this->username = $username;
+    }
+    
+    public function getPassword() : string
+    {
+        return $this->password;
+    }
+    public function setPassword(string $password) : void
+    {
+        $this->password = $password;
+    }
+    
+    public function getEmail() : string
+    {
+        return $this->email;
+    }
+    public function setEmail(string $email) : void
+    {
+        $this->email = $email;
+    }
+    
+    public function getId() : int
+    {
+        return $this->id;
+    }
+    public function setId(int $id) : void
+    {
+        $this->id = $id;
+    }
+    
+    public function addUser(PDO $db, User $user) : void
+    {
+        $query = $db->prepare("INSERT INTO users (email, username, password) 
+                       VALUES (?, ?, ?)");
+        $query->execute([$user->getEmail(), $user->getUsername(), $user->getPassword()]);
+    }
+    
+    public function removeUser(PDO $db, User $user) : void
+    {
+        $query = $db->prepare("DELETE FROM users
+                               WHERE users.username = :email");
+        $parameters = [
+            'email' => $user->getEmail()   
+        ];
+        $query->execute($parameters);
+    }
+    
+    public function addRole(PDO $db, User $user) : void
+    {
+        
+    }
+}
+
+// $marie = new User("Milyra", "bonjour", "marie@gmail.com");
+// echo "<pre>";
+// print_r($marie);
+// echo "<pre>";
+
+?>
